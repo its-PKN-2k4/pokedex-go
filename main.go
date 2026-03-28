6efc7a94f7d56d107bb7de451adbe17031f4b154
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-03-27 23:05:20 -0400

    Add read-eval-print loop

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	//print("Hello, World!\n")
-	processed := cleanInput("  hello    world    ")
-	for _, word := range processed {
-		print(word)
-		print("\n")
+	scanner := bufio.NewScanner(os.Stdin)
+	running := true
+	for running {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		fmt.Printf("Your command was: %v\n", cleanedInput[0])
 	}
 }
