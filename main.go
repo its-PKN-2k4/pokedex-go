8bfb55c4a6da89d88c7c90b548fc3de00c7482c1
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-03-31 02:49:36 -0400

    Add more cli commands for program

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	running := true
@@ -14,6 +20,46 @@ func main() {
 		scanner.Scan()
 		input := scanner.Text()
 		cleanedInput := cleanInput(input)
-		fmt.Printf("Your command was: %v\n", cleanedInput[0])
+		if cmd, exist := getCommands()[cleanedInput[0]]; exist {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Printf("Error: %v", err)
+			}
+		} else {
+			fmt.Println("Command not found")
+			continue
+		}
+	}
+}
+
+func commandExit() error {
+	print("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show how to use the Pokedex",
+			callback:    commandHelp,
+		},
 	}
 }
